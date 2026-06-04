47bb992f7f73520819a560e5a31f9983c45448d3
Alina Bakhvalova <a.bahvalova@inetstudio.ru>
2026-06-04 14:41:02 -0400

    Подготовка стилей, типизация define props

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import ProfileAvatar from './components/ProfileAvatar.vue'
+</script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-  </header>
+  <div class="app">
+    <nav class="nav"><ProfileAvatar name="Антон" /></nav>
+    <main>Контент</main>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.app {
+  display: flex;
+  gap: 300px;
+  max-width: 1460px;
+  padding: 140px 100px 0;
+  margin: 0 auto;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.nav {
+  min-width: 140px;
 }
 </style>
---
+++ src/components/ProfileAvatar.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+const { name } = defineProps<{ name: string }>()
+</script>
+
+<template>
+  <div class="profile-avatar">
+    <img
+      src="/src/assets/avatar.png"
+      alt="Изображение пользователя"
+      class="profile-avatar__image"
+    />
+    <div class="profile-avatar__text">
+      Привет, <span>{{ name }}!</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.profile-avatar {
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+}
+
+.profile-avatar__image {
+  width: 80px;
+  height: 80px;
+}
+
+.profile-avatar__text {
+  font-size: 18px;
+  line-height: 110%;
+  letter-spacing: 0.4px;
+}
+
+.profile-avatar__text > span {
+  font-weight: 700;
+}
+</style>
