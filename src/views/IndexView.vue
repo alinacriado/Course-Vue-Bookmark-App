71026867594931bf2e87948cc33fe74d00ab99af
Alina Bakhvalova <a.bahvalova@inetstudio.ru>
2026-06-09 18:04:25 -0400

    Упражнение - Главная страница

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -1,1 +1,28 @@
-<template>Main Page</template>
+<template>
+  <div class="index">
+    <h1 class="index__header">Добро пожаловать в Bookmarkly!</h1>
+    <p class="index__description">
+      Чтобы начать пользоваться выберите категорию закладок или создайте новую.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.index {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  gap: 25px;
+  min-height: 50vh;
+}
+.index__header {
+  font-size: 24px;
+  font-weight: 500;
+}
+.index__description {
+  font-size: 16px;
+  line-height: 110%;
+}
+</style>
